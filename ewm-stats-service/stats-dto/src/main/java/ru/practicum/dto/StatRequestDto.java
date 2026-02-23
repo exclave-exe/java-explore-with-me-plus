97831b1f27b16a02a5e102cb91f83178f332648e
ropgocTb <216320907+ropgocTb@users.noreply.github.com>
2026-02-23 16:56:52 +0300

    Added: ewm-stats-service module, stats-dto module, dtos for stat request and response

---
+++ ewm-stats-service/stats-dto/src/main/java/ru/practicum/dto/StatRequestDto.java
@@ -0,0 +1,26 @@
+package ru.practicum.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class StatRequestDto {
+    @NotBlank
+    private String app;
+
+    @NotBlank
+    private String uri;
+
+    @NotBlank
+    private String ip;
+
+    @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+}
