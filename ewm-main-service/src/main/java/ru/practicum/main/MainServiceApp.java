6f19c1a58f2c29047d290494e047a07be1f64a46
ropgocTb <216320907+ropgocTb@users.noreply.github.com>
2026-03-02 01:15:35 +0300

    properties and docker-compose

--- ewm-main-service/src/main/java/ru/practicum/main/MainServiceApp.java
+++ ewm-main-service/src/main/java/ru/practicum/main/MainServiceApp.java
@@ -1,9 +1,11 @@
-package ru.practicum;
+package ru.practicum.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "ru.practicum")
 public class MainServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(MainServiceApp.class, args);
